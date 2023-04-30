b8d07ac368eb46f63b6cf0b691e928511dca8185
Paul Nickerson <pvnick>
2023-04-30 13:42:06 -0400

    ipam delegated administrator

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
-resource "aws_organizations_delegated_administrator" "networking" {
-  account_id        = var.networking-account
-  service_principal = "principal"
+# resource "aws_organizations_delegated_administrator" "networking" {
+#   account_id        = var.networking-account
+#   service_principal = "principal"
+# }
+
+resource "aws_vpc_ipam_organization_admin_account" "example" {
+  delegated_admin_account_id = var.networking-account
 }
