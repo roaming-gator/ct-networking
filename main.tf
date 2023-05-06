298bfe3ac9a8586231b3abed9469138a5dce8470
Paul Nickerson <pvnick>
2023-05-05 21:34:55 -0400

    clean up debug output

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
 resource "aws_vpc_ipam_organization_admin_account" "networking" {
   delegated_admin_account_id = var.networking-account
 }
-
-data "aws_organizations_organization" "org" {}
-
-data "aws_organizations_organizational_units" "ou" {
-  parent_id = data.aws_organizations_organization.org.roots[0].id
-}
--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "roots" {
-  value = data.aws_organizations_organization.org.roots
-}
-
-output "units" {
-  value = data.aws_organizations_organizational_units.ou.children
-}
